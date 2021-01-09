cbbd6b756b5f4a721ba630d10496c4a5e64b659f
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 11:55:46 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,3 +17,12 @@ def output_array(array)
     counter += 1
   end
 end
+
+def return_string_array(array)
+  new_array = []
+  counter = 0 
+  while array[counter] do
+    new_array
+    counter += 1
+  end
+  
