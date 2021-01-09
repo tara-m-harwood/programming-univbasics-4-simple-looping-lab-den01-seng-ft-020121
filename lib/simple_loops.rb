9b87b7a3d37aa76cbd48f73029288342c0c7586a
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 11:59:29 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
-# Write your methods here
+def loop_message_five_times(message)
+  5.times do
+    puts message
+  end
+end
+
+def loop_message_n_times(message, number)
+  number.times do
+    puts message
+  end
+end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
+
+def return_string_array(array)
+  new_array = []
+  index = 0 
+  while array[index] do
+    new_array[index] = array[index].to_s
+    index += 1
+  end
+  new_array
+end
+  
