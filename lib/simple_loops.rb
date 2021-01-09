4736f02cebb7e78d921852d318f733078a13456a
Tara Harwood <tara.m.harwood@gmail.com>
2021-01-09 11:59:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -22,7 +22,7 @@ def return_string_array(array)
   new_array = []
   index = 0 
   while array[index] do
-    new_array[index] = array[index]
+    new_array[index] = array[index].to_s
     index += 1
   end
   new_array
